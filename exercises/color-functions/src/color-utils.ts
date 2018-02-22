cbd7d8f667623d6b4004a56bdaac15236451e431
Aric Alves <aric.alves@yahoo.com>
2018-02-21 16:51:43 -0800

    finish color funcitons

--- exercises/color-functions/src/color-utils.ts
+++ exercises/color-functions/src/color-utils.ts
@@ -1,4 +1,17 @@
-//TODO: Implement hexToRgb
+export function hexToRgb(hex: string): {r:number, g:number, b:number} {
+  if (hex.length === 3) {
+    return hexToRgb(`${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}`)
+  }
 
+  let [r, g, b] = [0, 2, 4]
+    .map(offset => hex.substring(offset, offset + 2))
+    .map(hexCode => parseInt(hexCode, 16))
+  return { r, g, b }
+}
 
-//TODO: Implement rgbToHex
+export function rgbToHex(r: number, g: number, b: number): string {
+  return [r, g, b]
+    .map(code => Math.max(0, Math.min(255, code)).toString(16))
+    .map(hex => hex.length < 2 ? `0${hex}` : hex)
+    .join('')
+}
