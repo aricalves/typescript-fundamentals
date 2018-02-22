0e482e9b535078cad18ab1053e877f18e4921173
Aric Alves <aric.alves@yahoo.com>
2018-02-21 20:36:38 -0800

    Finish cashier

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,3 +1,36 @@
-// export function cashier() {
-//   return {};
-// }
+interface CartAPI {
+  length: number
+  total: number
+  addItem (item: CartItem): CartAPI
+  add (name: string, price: number, qty: number): CartAPI
+}
+
+interface CartItem {
+  name: string,
+  price: number,
+  qty: number
+}
+
+export function cashier(): CartAPI {
+  let items: CartItem[] = []
+  return {
+    get length () {
+      return items.reduce((acc, item) => {
+        return acc + item.qty
+      }, 0)
+    },
+    get total () {
+      return items.reduce((acc, item) => {
+        return acc + (item.qty * item.price)
+      }, 0)
+    },
+    addItem (item): CartAPI {
+      items.push(item)
+      return this;
+    },
+    add (name, price, qty = 1): CartAPI {
+      items.push({ name, price, qty })
+      return this;
+    }
+  };
+}
