4148f229f97d32a28f0de68f5094dceff9c9ef18
Aric Alves <aric.alves@yahoo.com>
2018-03-02 12:20:51 -0800

    Finish fib

--- exercises/fib/src/fib.ts
+++ exercises/fib/src/fib.ts
@@ -3,6 +3,13 @@
  * Implement a generator function that can be used
  * to generate numbers in the Fibonacci Sequence
  */
-// export function* getFibSequence(): IterableIterator<number> {
- 
-// }
+export function* getFibSequence(): IterableIterator<number> {
+  let lastLast = 1
+  let last = 0
+  while (1) {
+    let val = last + lastLast
+    yield val
+    lastLast = last
+    last = val
+  }
+}
